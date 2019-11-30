9bb8c598ddd23948185a261251784b41edb98e34
Morderon <will386@gmail.com>
2019-11-30 11:32:04 -0500

    Test 2

--- Plugins/Arelith/Source/ArelithEvents.hpp
+++ Plugins/Arelith/Source/ArelithEvents.hpp
@@ -17,7 +17,7 @@ private:
     static int32_t CanUseItemHook(CNWSCreature *pCreature, CNWSItem *pItem, int32_t bIgnoreIdentifiedFlag);
     static unsigned char CanEquipWeaponHook( CNWSCreature *pCreature, CNWSItem *pItem, int32_t *nEquipToSlot, int32_t bEquipping, int32_t, CNWSPlayer *pFeedbackPlayer);
     static unsigned char CanUnEquipWeaponHook( CNWSCreature *pCreature, CNWSItem *pItem);
-    static int32_t OnApplyDisarmHook(NCNWSEffectListHandler*, CNWSObject *pObject, CGameEffect *pEffect, int32_t bLoadingGame);
+    static int32_t OnApplyDisarmHook(CNWSEffectListHandler*, CNWSObject *pObject, CGameEffect *pEffect, int32_t bLoadingGame);
     static int32_t OnEffectAppliedHook(CNWSEffectListHandler *pEffectListHandler, CNWSObject *pObject, CGameEffect *pEffect, int32_t bLoadingGame);
 };
 
