28e7684f522290a9e49b0698509e9d28c736c106
Liareth <liarethnwn@gmail.com>
2022-08-15 16:24:50 +0100

    Add CustomAlloc plugin - doesn't actually do anything, but overwrites malloc et all when preloaded.

---
+++ Plugins/CustomAlloc/CustomAlloc.cpp
@@ -0,0 +1,25 @@
+#define VMEM_DEBUG_LEVEL 0
+#include "VMem.cpp"
+
+extern "C" void* malloc(size_t size)
+{
+	return VMem::Alloc(size);
+}
+
+extern "C" void* calloc(size_t num, size_t size)
+{
+	int total = num * size;
+	void* ptr = VMem::Alloc(total);
+	memset(ptr, 0, total);
+	return ptr;
+}
+
+extern "C" void* realloc(void* ptr, size_t size)
+{
+	return VMem::Realloc(ptr, size);
+}
+
+extern "C" void free(void* ptr)
+{
+	VMem::Free(ptr);
+}
