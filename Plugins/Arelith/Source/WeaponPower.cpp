e23fa15436fe836f0179cbe58b2c0842557706fe
Silvard <arelith@silvard.net>
2022-08-30 22:25:09 -0400

    Enable override to weapon power for DR piercing

---
+++ Plugins/Arelith/Source/WeaponPower.cpp
@@ -0,0 +1,35 @@
+#include "nwnx.hpp"
+
+#include "API/CNWSCreature.hpp"
+#include "API/CNWSItem.hpp"
+#include "API/CNWSCombatRound.hpp"
+
+namespace Arelith {
+
+using namespace NWNXLib;
+using namespace NWNXLib::API;
+
+static Hooks::Hook s_getWeaponPower;
+
+//called whenever weapon power for DR piercing is calculated
+int32_t WeaponPowerHook(CNWSCreature* thisPtr, CNWSObject* pTarget, int32_t bOffhand)
+{
+    static CExoString s_WeaponPowerOverride = "WEAPON_POWER_OVERRIDE";
+    CNWSItem* pAttackWeapon = nullptr;
+    int32_t nWeaponPower = 0;
+
+    pAttackWeapon = (bOffhand) ? thisPtr->m_pcCombatRound->GetCurrentAttackWeapon(2) :  thisPtr->m_pcCombatRound->GetCurrentAttackWeapon(0);
+
+    nWeaponPower = (int32_t)pAttackWeapon->m_ScriptVars.GetInt(s_WeaponPowerOverride);
+    int32_t nNormalPower = s_getWeaponPower->CallOriginal<int32_t>(thisPtr, pTarget, bOffhand);
+
+    return (nWeaponPower > nNormalPower) ? nWeaponPower : nNormalPower;
+}
+
+void WeaponPower() __attribute__((constructor));
+
+void WeaponPower()
+{
+    s_getWeaponPower = Hooks::HookFunction(Functions::_ZN12CNWSCreature14GetWeaponPowerEP10CNWSObjecti, (void*)&WeaponPowerHook, Hooks::Order::Final);
+}
+}
